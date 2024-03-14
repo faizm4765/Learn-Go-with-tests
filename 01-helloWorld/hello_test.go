fd18f0066555df38bd91ed431c9aa85a61a71904
faizmohammad <faizmohammad@mcirosoft.com>
2024-03-15 02:09:19 +0530

    use %q format specifier

--- 01-helloWorld/hello_test.go
+++ 01-helloWorld/hello_test.go
@@ -5,7 +5,8 @@ import "testing"
 func TestHello(t *testing.T) {
 	got := Hello()
 	want := "Hello world!"
+
 	if got != want {
-		t.Errorf("got %s want %q", got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
