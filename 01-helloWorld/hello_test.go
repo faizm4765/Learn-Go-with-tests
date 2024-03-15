fe0f17e69ec9701cbc1a8f6ce90e18aa594c2ddc
faizmohammad <faizmohammad@mcirosoft.com>
2024-03-15 23:01:53 +0530

    updated test file to include dynamic name

--- 01-helloWorld/hello_test.go
+++ 01-helloWorld/hello_test.go
@@ -3,10 +3,12 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello world!"
+	t.Run("saying Hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello Chris!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
