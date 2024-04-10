fe560479f37506aa07976fc9a95a95fff38597d3
faizmohammad <faizmohammad@mcirosoft.com>
2024-04-11 01:24:26 +0530

    added test case for French

--- 01-helloWorld/hello_test.go
+++ 01-helloWorld/hello_test.go
@@ -17,9 +17,15 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("in spanish", func(t *testing.T) {
+	t.Run("in Spanish", func(t *testing.T) {
 		got := Hello("Elodie", "Spanish")
 		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Pam", "French")
+		want := "Bonjour, Pam"
 		assertCorrectMessage(t, got, want)
 	})
 }
