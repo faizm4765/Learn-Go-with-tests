d8a6b3d7531f729ea6e355d76ec35657ae6ff164
faizmohammad <faizmohammad@microsoft.com>
2024-10-06 17:15:47 +0530

    added test file

--- 03-dependency-injection/dependency.go
+++ 03-dependency-injection/dependency.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func Greet(name string) {
+	fmt.Printf("Hello, %s", name)
+}
+
 func main() {
-	fmt.Printf("Learning dependency injection")
+	fmt.Printf("Learning dependency injection\n")
+	Greet("Faiz")
 }
--- 03-dependency-injection/dependency_test.go
+++ 03-dependency-injection/dependency_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	bytes := bytes.Buffer{}
+	got := ""
+	want := "Hello, Faiz"
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
