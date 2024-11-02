37caca769c8a2e96feff62960ca4e5e38b7af690
faizmohammad <faizmohammad@microsoft.com>
2024-11-02 19:32:39 +0530

    created new directory for next mocking example

---
+++ 05-mockingII/mocking.go
@@ -0,0 +1,9 @@
+package main
+
+// Is file mein humlog stretch krenge mocking method ko to see ki Print and Sleep ka order sahi follow ho ra ya nhin. Ye extended example hai 04-mocking ka.
+
+import "fmt"
+
+func main() {
+	fmt.Println("*&54")
+}
