767530e5d3e06cff95104ec583e15f68aeaf7451
faizmohammad <faizmohammad@microsoft.com>
2024-11-02 17:15:38 +0530

    test passing

--- 04-mocking/mocking.go
+++ 04-mocking/mocking.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func Countdown(w io.Writer) {
+	fmt.Fprintln(w, 3)
+	fmt.Fprintln(w, 2)
+	fmt.Fprintln(w, 1)
+	fmt.Fprintln(w, "Go!")
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
