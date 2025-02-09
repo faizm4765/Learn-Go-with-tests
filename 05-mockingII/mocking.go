36294fbb3605c1e198013046a4bf3037241849ef
faizmohammad <faizmohammad@microsoft.com>
2025-02-09 20:47:57 +0530

    test commit

--- 05-mockingII/mocking.go
+++ 05-mockingII/mocking.go
@@ -1,6 +1,7 @@
 package main
 
 // Is file mein humlog stretch krenge mocking method ko to see ki Print and Sleep ka order sahi follow ho ra ya nhin. Ye extended example hai 04-mocking ka.
+// kya ye aaj krengee????
 
 import "fmt"
 
