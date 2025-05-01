c005fa4328a541a2321625ef7bfbb6be8f09ddb8
faizm4765 <faizm4765@gmail.com>
2025-05-01 20:50:39 +0530

    mocked api server

--- 05-mockingII/mocking_test.go
+++ 05-mockingII/mocking_test.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestUserNotFound(t *testing.T) {
-	baseUrl := "https://jsonplaceholder.typicode.com/"
-	client := NewApiClient(baseUrl)
+	// baseUrl := "https://jsonplaceholder.typicode.com/"
+	mocksServer := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Error(w, "Not Found", http.StatusNotFound)
+		}),
+	)
+
+	client := NewApiClient(mocksServer.URL)
 	statusCode, _ := client.FetchUser(1)
 	expectedStatusCode := 404
 	if statusCode != expectedStatusCode {
