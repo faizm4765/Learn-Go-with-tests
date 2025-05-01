2abdf7f0fcff352cae3ce136d90032a2490f5b3c
faizm4765 <faizm4765@gmail.com>
2025-05-01 21:16:02 +0530

    added second test case

--- 05-mockingII/mocking.go
+++ 05-mockingII/mocking.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 type ApiClient struct {
 	baseUrl string
+}
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 func NewApiClient(baseUrl string) ApiClient {
@@ -24,6 +31,32 @@ func (client ApiClient) FetchUser(id int) (int, error) {
 
 	defer resp.Body.Close()
 	return resp.StatusCode, err
+}
+
+func (client ApiClient) FetchAllUsers() ([]User, error) {
+	url := fmt.Sprintf("%s/users", client.baseUrl)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching users:", err)
+		return []User{}, err
+	}
+
+	defer resp.Body.Close()
+	resp_body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return []User{}, err
+	}
+
+	var users []User
+	err = json.Unmarshal(resp_body, &users)
+	if err != nil {
+		fmt.Println("Error unmarshalling response body:", err)
+		return []User{}, err
+	}
+
+	return users, nil
 }
 
 func main() {
--- 05-mockingII/mocking_test.go
+++ 05-mockingII/mocking_test.go
@@ -21,3 +21,27 @@ func TestUserNotFound(t *testing.T) {
 		t.Errorf("Got %d Expected %d", statusCode, expectedStatusCode)
 	}
 }
+
+func TestUsersListResponse(t *testing.T) {
+	// baseUrl := "https://jsonplaceholder.typicode.com/"
+	mocksServer := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			jsonOutput := `
+				[
+					{"id": 1, "name": "John Doe"},
+					{"id": 2, "name": "Alex Doe"},
+					{"id": 3, "name": "Harry Doe"}
+				]`
+
+			w.Write([]byte(jsonOutput))
+		}),
+	)
+
+	client := NewApiClient(mocksServer.URL)
+	users, _ := client.FetchAllUsers()
+	expectedUsersCount := 3
+	if len(users) != 3 {
+		t.Errorf("Got %d Expected %d", len(users), expectedUsersCount)
+	}
+}
